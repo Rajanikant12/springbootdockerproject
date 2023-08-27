8f478349369cbabf7784dbc289f10d8ef626b4b0
Rajnikant Yadav <rkgecrajni321@gmail.com>
2023-08-27 14:58:23 +0530

    test1

--- src/main/java/com/springboot/dockerproject/SpringbootdockerprojectApplication.java
+++ src/main/java/com/springboot/dockerproject/SpringbootdockerprojectApplication.java
@@ -9,6 +9,7 @@ public class SpringbootdockerprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdockerprojectApplication.class, args);
 		System.out.println("Hello World Docker Image");
+		System.out.println("Hi I am learning Docker");
 	}
 
 }

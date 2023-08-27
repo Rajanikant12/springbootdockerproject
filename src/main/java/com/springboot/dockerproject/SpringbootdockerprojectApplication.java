3718f683e20861d6dbf261aaff663cc0615d0df5
Rajnikant Yadav <rkgecrajni321@gmail.com>
2023-08-27 14:33:21 +0530

    first commit

---
+++ src/main/java/com/springboot/dockerproject/SpringbootdockerprojectApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.dockerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdockerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdockerprojectApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/springboot/dockerproject/SpringbootdockerprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.dockerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdockerprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
